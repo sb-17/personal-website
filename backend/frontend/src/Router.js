21ed4784fb6187c01b376c038ae69e69994b1606
simon <simon.borovsky@gmail.com>
2021-11-10 21:11:40 +0100

    heroku

--- backend/frontend/src/Router.js
+++ backend/frontend/src/Router.js
@@ -3,18 +3,18 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home';
-import Navbar from "./components/Navbar";
 import ShowProjectList from './components/ShowProjectList';
 import ShowProjectDetails from './components/ShowProjectDetails';
 import AdminPanel from './components/AdminPanel';
 import About from './components/About';
+import NavigationBar from './components/NavigationBar';
 
 function Router() {
 
   return (
     <BrowserRouter>
       <div>
-        <Navbar />
+        <NavigationBar />
         <Route exact path='/' component={Home} />
         <Route path='/projects' component={ShowProjectList} />
         <Route path='/project/:id' component={ShowProjectDetails} />
---
+++ backend/frontend/src/components/NavigationBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+function NavigationBar() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/">Šimon Borovský</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/projects">Projects</Nav.Link>
+        <Nav.Link href="/about">About me</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
