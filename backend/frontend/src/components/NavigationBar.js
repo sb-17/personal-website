a7adc5c976aceeaf332a36fa8c9e16bd14e4a622
sb-17 <88972742+sb-17@users.noreply.github.com>
2021-12-01 10:46:30 +0100

    logout

--- backend/frontend/src/components/NavigationBar.js
+++ backend/frontend/src/components/NavigationBar.js
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import { reactLocalStorage } from 'reactjs-localstorage';
 
-function NavigationBar() {
-  return (
-    <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">Šimon Borovský</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/about">About me</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    };
+  }
+
+  componentDidMount() {
+    const header = {
+      headers: {
+        'Authorization': reactLocalStorage.get('token')
+      }
+    }
+
+    axios.post('/api/auth', null, header).then(response => {
+      if (response.data.data.user) {
+        this.setState({ loggedIn: true });
+      }
+    });
+  };
+
+  logout() {
+    const header = {
+      headers: {
+        'Authorization': reactLocalStorage.get('token')
+      }
+    }
+
+    axios.post('/api/auth', null, header).then(response => {
+      if (response.data.data.user) {
+        reactLocalStorage.remove('token');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Šimon Borovský</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/projects">Projects</Nav.Link>
+              <Nav.Link href="/about">About me</Nav.Link>
+            </Nav>
+            {
+              this.state.loggedIn &&
+              <Nav className="ml-auto">
+                <Nav.Link onClick={logout()}>Log out</Nav.Link>
+              </Nav>
+            }
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
 export default NavigationBar;
