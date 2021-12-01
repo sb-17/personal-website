61d20362ddc8a14bd1a338250ca362173fce8f59
simon <simon.borovsky@gmail.com>
2021-12-01 15:16:10 +0100

    ui

--- backend/frontend/src/components/CreateProject.js
+++ backend/frontend/src/components/CreateProject.js
@@ -84,10 +84,9 @@ class CreateProject extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <br />
               <h1 className="display-4 text-center">Add Project</h1>
-              <p className="lead text-center">
-                Create new project
-              </p>
+              <hr />
 
               <form noValidate onSubmit={this.onSubmit}>
                 <div className='form-group'>
