d857aea37857a79fa38b7070b4611c503d3874b3
simon <simon.borovsky@gmail.com>
2021-12-11 13:16:08 +0100

    fix

--- backend/frontend/src/components/Register.js
+++ backend/frontend/src/components/Register.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import '../App.css';
+import { reactLocalStorage } from 'reactjs-localstorage';
 
 class Register extends Component {
     constructor() {
@@ -16,17 +17,17 @@ class Register extends Component {
 
     componentDidMount() {
         const header = {
-          headers: {
-            'Authorization': reactLocalStorage.get('token')
-          }
+            headers: {
+                'Authorization': reactLocalStorage.get('token')
+            }
         }
-    
+
         axios.post('/api/auth', null, header).then(response => {
-          if (response.data.data.user) {
-            this.props.history.push('/');
-          }
+            if (response.data.data.user) {
+                this.props.history.push('/');
+            }
         }).catch(err => {
-          this.props.history.push('/');
+            this.props.history.push('/');
         });
     }
 
@@ -42,11 +43,11 @@ class Register extends Component {
                 username: this.state.username,
                 password: this.state.password
             };
-    
+
             axios.post('/api/auth/register', data).then(response => {
                 this.props.history.push('/login');
             });
-    
+
             this.setState({ isSubmitted: true });
         }
         else {
